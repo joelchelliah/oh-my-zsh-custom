656e04d9e96f24867b4bbde0606846204e55b5d6
Joel Chelliah <joel.chelliah@bekk.no>
2017-02-17 13:58:01 +0100

    First commit

---
+++ git-aliases.zsh
@@ -0,0 +1,7 @@
+# Custom / Overridden git aliases
+
+alias gcm='git commit -m'
+alias gp='git pull'
+alias gps='git push'
+alias gs='git status -s'
+alias gaa="git add .; echo -e '\nAdded all changes\n'; gs"
