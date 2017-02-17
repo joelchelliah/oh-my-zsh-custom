da714396d44bcc751ea5c7f878ad00fd68a41576
Joel Chelliah <joel.chelliah@bekk.no>
2017-02-17 14:03:38 +0100

    Added git aliases

--- git-aliases.zsh
+++ git-aliases.zsh
@@ -1,7 +1,10 @@
 # Custom / Overridden git aliases
 
 alias gcm='git commit -m'
-alias gp='git pull'
+alias gp='git pull --prune'
 alias gps='git push'
 alias gs='git status -s'
-alias gaa="git add .; echo -e '\nAdded all changes\n'; gs"
+alias gaa="git add .; echo -e '\nAdded all changes\n'; git status -s"
+alias gun="git undo; echo -e '\nUndid last commit\n'; git status -s"
+alias gl="git log --pretty=oneline -n 20 --graph --abbrev-commit"
+alias gh="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen | %ad (%cr) | %C(bold blue)<%an>%Creset' --abbrev-commit --date=short"
