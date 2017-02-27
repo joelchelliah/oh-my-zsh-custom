526cf64fb90603f58cf26b5926db313ad2dc156a
Joel Chelliah <joel.chelliah@bekk.no>
2017-02-27 08:26:51 +0100

    function for selecting jvm version

--- git-aliases.zsh
+++ git-aliases.zsh
@@ -8,3 +8,6 @@ alias gaa="git add .; echo -e '\nAdded all changes\n'; git status -s"
 alias gun="git undo; echo -e '\nUndid last commit\n'; git status -s"
 alias gl="git log --pretty=oneline -n 20 --graph --abbrev-commit"
 alias gh="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen | %ad (%cr) | %C(bold blue)<%an>%Creset' --abbrev-commit --date=short"
+
+# Commit with branch name in title
+gcwb () { git commit -m "$(git rev-parse --abbrev-ref HEAD): $1"; }
