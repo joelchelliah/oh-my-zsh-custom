f95fda9f60e3834cce83b2f9ed98ca8dd4346d11
Joel Chelliah <joel.chelliah@bekk.no>
2017-03-01 20:39:46 +0100

    Added paths and exports

--- paths.zsh
+++ paths.zsh
@@ -1,3 +1,31 @@
-## --------------------------- JAVA -----------------------------------
+## ---------------------------- BIN ---------------------------
+export BIN=$HOME/bin
+
+export PATH=$BIN:$PATH
+
+
+## --------------------------- JAVA ---------------------------
 # export JAVA_HOME=$(/usr/libexec/java_home)
 export JAVA_HOME="`/usr/libexec/java_home -v '1.7*'`"
+
+
+## --------------------------- SCALA --------------------------
+export SCALA_HOME=$BIN/scala-2.11.7
+
+export PATH=$SCALA_HOME/bin:$PATH
+
+
+## ------------------------- HASKELL --------------------------
+export HASKELL_HOME=$HOME/Library/Haskell
+
+export PATH=$HASKELL_HOME/bin:$PATH
+
+
+## --------------------------- STACK --------------------------
+export STACK_INSTALL_PATH=$HOME/.local/bin
+
+export PATH=$BIN/stack-1.0.4-osx-x86_64:$STACK_INSTALL_PATH:$PATH
+
+
+## ---------------------------- GO ----------------------------
+export GOPATH=$HOME/src
