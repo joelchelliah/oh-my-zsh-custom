0279d35e7fd9b3e8c077e192965ab19f605a24fb
Joel Chelliah <joel.chelliah@bekk.no>
2017-03-08 08:00:47 +0100

    Added sshconfig alias

--- general-aliases.zsh
+++ general-aliases.zsh
@@ -2,4 +2,8 @@
 
 alias zshconfig="atom ~/.zshrc"
 alias zshsource="source ~/.zshrc"
-alias ohmyzsh="atom ~/.oh-my-zsh"
+
+alias ohmyzsh="cd ~/.oh-my-zsh"
+alias ohmyzshconfig="atom ~/.oh-my-zsh"
+
+alias sshconfig="atom ~/.ssh/config"
