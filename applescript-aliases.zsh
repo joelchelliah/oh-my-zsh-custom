f9fdd2ca37bc2c0dbbba63ef6418ff98fd2430f7
Joel Chelliah <joel.chelliah@bekk.no>
2019-06-21 11:58:57 +0200

    alias for backing up iTerm scripts

--- applescript-aliases.zsh
+++ applescript-aliases.zsh
@@ -1,6 +1,10 @@
-# ZSH
+# Edit iTerm scripts
+alias itermConfig="code ~/Library/Application\ Support/iTerm2/Scripts"
 
-alias itermconfig="code ~/Library/Application\ Support/iTerm2/Scripts"
+# Backup iTerm scripts in iCloud
+alias itermBackup="cp ~/Library/Application\ Support/iTerm2/Scripts/* ~/Documents/scripts/iTerm2-scripts"
 
+# Run iTerm scripts
 alias darkish="darkishTab.scpt"
 alias enclients="enclientsTab.scpt"
+alias enbff="bffTab.scpt"
