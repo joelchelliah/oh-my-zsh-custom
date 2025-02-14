de4dfa7a2524d32caa13586b281fdc023406e711
Joel Chelliah <joel.chelliah@bekk.no>
2025-02-14 12:33:44 +0100

    Updated vpn-split

--- plugins/vpn-split/vpn_split.py
+++ plugins/vpn-split/vpn_split.py
@@ -3,16 +3,20 @@ import os
 import re
 import subprocess
 import sys
+from datetime import datetime
 
 WIRELESS_INTERFACE = 'en0'    # could be different on other systems
 TUNNEL_INTERFACE = 'utun2'
 
 VPN_NETS = [
+    "10.94.36",
+    "10.242.133"
 ]
 
 VPN_HOSTS = [
-    '10.94.36.24',  # ocp.test.domstol.no
-    '10.94.36.25',  # ocp.at.domstol.no
+#     '10.94.36.24',    # ocp.test.domstol.no
+#     '10.94.36.25',    # ocp.at.domstol.no
+#     '10.242.133.104', # domstolatsql107.at.domstol.no
 ]
 
 
@@ -21,8 +25,17 @@ def get_tunnel_interface():
     for line in reversed(subprocess.check_output(["ifconfig"]).decode("utf-8").splitlines()):
         match = re.match(r"(utun\d)", line)
         if match:
-            return match.groups(1)[0]
+            interface = match.groups(1)[0]
 
+            if get_protocols(interface) == ["inet"]:
+                return interface
+
+def get_protocols(interface):
+    lines = subprocess.check_output(["ifconfig", interface]).decode("utf-8").splitlines()
+
+    protocols = [line.strip().split(" ")[0] for line in lines[1:]]
+
+    return protocols
 
 def split_vpn_traffic():
     if os.getuid() != 0:
@@ -43,7 +56,7 @@ def split_vpn_traffic():
     if gateway is None:
         sys.exit("Unable to determine VPN default gateway.")
 
-    print("Resetting routes with gateway " + gateway)
+    print("\nResetting routes with gateway " + gateway + "\n")
     subprocess.call(
         ("route", "-n", "delete", "default", "-ifscope", WIRELESS_INTERFACE)
     )
@@ -52,7 +65,9 @@ def split_vpn_traffic():
     )
     subprocess.call(("route", "-n", "add", "-net", "default", gateway))
 
-    print("\nAdding routes for addresses which should go through VPN.")
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"\n[{current_time}] Adding routes for addresses which should go through VPN:\n")
+
     for addr in VPN_NETS:
         subprocess.call(
             ("route", "-n", "add", "-net", addr, "-iface", tunnel_interface)
